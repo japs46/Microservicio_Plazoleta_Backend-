35267e6afb058aa81f077f55f609bd4a4979db7b
Jhonathan <jhonathanps46@gmail.com>
2024-09-23 20:13:11 -0500

    Commit Inicial

---
+++ src/main/java/com/pragma/backend/MicroservicioPlazoletaApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioPlazoletaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioPlazoletaApplication.class, args);
+	}
+
+}
